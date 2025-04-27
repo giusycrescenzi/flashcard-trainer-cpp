289feda8579199ea7d6c2b4cdd9aa7400d49a1fa
Giuseppe Crescenzi <giuseppecrescenzi17p@gmail.com>
2025-04-27 19:42:57 +0200

    further implementations in gamemanager class

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,17 +1,54 @@
 #include "GameManager.h"
 
-void GameManager::SetPlayers(int p) {
+void GameManager::SetNumPlayers(int p) {
     players = p;
 }
-int GameManager::GetPlayers() const {
+int GameManager::GetNumPlayers() const {
     return players;
 }
-
-void GameManager::start() {
-
+void GameManager::SetPlayerName(int n,const std::string& name) {
+    names[n] = name;
+}
+std::string GameManager::GetPlayerName(int n,const std::string& name) const {
+    return names[n];
 }
 
-void GameManager::DisplayHelp() {
-    std::cout << "[ Commands ]"
-        << "H" <<std::endl;
+void GameManager::run() {
+    bool running = true;
+    while (running) {
+        // first thing is getting every player info in the manager
+        do {
+            std::cout << "insert number of players ";
+            std::cin >> players;
+        } while (players < 1);
+        if (players == 1) {
+            SetPlayerName(0, "You");
+            std::cout << "There we go, best of luck pal" << std::endl;
+        }
+        else {
+            std::cout << "game for " << players << " initialized, good luck";
+            for (int i = 0; i < players; i++) {
+                std::string name;
+                std::cout << "player number " << i+1 << " insert your name";
+                std::getline (std::cin, name);
+                std::cout << name << " added" << std::endl;
+                SetPlayerName (i, name);
+            }
+        }
+    }
+    
+}
+
+void GameManager::DisplayHelp() const{
+    std::cout << "[ Commands ]" << std::endl;
+    std::cout << "help          if you need to see this again"<< std::endl;
+    std::cout << "about         show app info" << std::endl;
+    std::cout << "score         show every player and his score" << std::endl;
+    std::cout << "restart       to start a new game" << std::endl;
+    std::cout << "quit          to exit the game" << std::endl;
+}
+void GameManager::DisplayScore() const {
+    for (int i = 0; i < players; i++) {
+        std::cout<<names[i]<<": ";
+    }
 }
--- src/GameManager.h
+++ src/GameManager.h
@@ -7,13 +7,25 @@
 class GameManager {
     public:
         GameManager();
-        void start();
+        void run();
 
-        void SetPlayers(int p);
+        void SetNumPlayers(int p);
+        void SetPlayerName(int n,const std::string& name); //NB takes the player index
+        void SetScore(int n, int s);
     private:
         int players;
-        int GetPlayers() const;
-        void DisplayHelp();
+        int* Score;
+        std::vector<std::string> names;
+        int GetNumPlayers() const;
+        std::string GameManager::GetPlayerName(int n,const std::string& name) const;
+        int GetScore(int n, int s) const;
+        
+
+        // all commands will be private, calls will be handled at run time by run method
+        void DisplayHelp() const;
+        void DisplayScore() const;
+        void SkipQuestion() const;
+        void restart(int n, std::vector<Deck>& decks);
 };
 
 #endif //GAMEMANAGER_H
